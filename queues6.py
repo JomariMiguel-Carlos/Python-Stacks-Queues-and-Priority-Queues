c8970be95b86f77e2657cc0011e481eb2badf1da
JomariMiguel-Carlos <jomarimadla0@gmail.com>
2022-12-13 14:55:08 +0800

    both queues6 and 3 have the same problem if i figure out one of this then i can do the rest

---
+++ queues6.py
@@ -0,0 +1,13 @@
+class PriorityQueue:
+    def __init__(self):
+        self._elements = []
+
+    def enqueue_with_priority(self, priority, value):
+        heappush(self._elements, (-priority,value))
+
+    def dequeue(self):
+        return heappop(self._elements)[1]
+
+print(messages.dequeue())
+print(messages.dequeue())
+print(messages.dequeue())
