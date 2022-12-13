3e4ad5b572191aac6a2415cd9d4a477e1be3ae69
JomariMiguel-Carlos <jomarimadla0@gmail.com>
2022-12-13 15:06:43 +0800

    done fixing error codes

--- queues3.py
+++ queues3.py
@@ -1,7 +1,34 @@
+from collections import deque
+
+class Queue:
+    def __init__(self, *elements):
+        self._elements = deque(elements)
+
+    def __len__(self):
+        return len(self._elements)
+
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
+
+    def enqueue(self, element):
+        self._elements.append(element)
+
+    def dequeue(self):
+        return self._elements.popleft()
+
+fifo = Queue ("1st", "2nd", "3rd")
+
+for elements in fifo:
+    print(elements)
+
+
 class Stack(Queue):
     def dequeue(self):
         return self._elements.pop()
 
 lifo = Stack("1st", "2nd", "3rd")
+print("")
+print("Then pop or reverse then this will be the order:")
 for element in lifo:
     print(element)
--- queues6.py
+++ queues6.py
@@ -1,3 +1,27 @@
+from collections import deque
+from heapq import heappop,heappush
+
+class PriorityQueue:
+    def __init__(self):
+        self._elements = []
+
+    def enqueue_with_priority(self, priority, value):
+        heappush(self._elements,(priority,value))
+
+    def dequeue(self):
+        return heappop(self._elements)
+
+CRITICAL = 3
+IMPORTANT = 2
+NEUTRAL = 1
+
+messages = PriorityQueue()
+messages.enqueue_with_priority(IMPORTANT,"Windshields wipers turned on")
+messages.enqueue_with_priority(NEUTRAL,"Radio station tuned in")
+messages.enqueue_with_priority(CRITICAL,"Brake pedal depressed")
+messages.enqueue_with_priority(IMPORTANT,"Hazard lights turned on")
+
+
 class PriorityQueue:
     def __init__(self):
         self._elements = []
@@ -8,6 +32,7 @@ class PriorityQueue:
     def dequeue(self):
         return heappop(self._elements)[1]
 
+print(messages.dequeue())
 print(messages.dequeue())
 print(messages.dequeue())
 print(messages.dequeue())
