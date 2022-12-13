4207953525aefaaa91ccca23ed1bfe746066851f
JomariMiguel-Carlos <jomarimadla0@gmail.com>
2022-12-13 13:30:12 +0800

    Done queueu.py

--- queues.py
+++ queues.py
@@ -1,16 +1,14 @@
 from collections import deque
 
 class Queue:
-    def __init__(self):
-        self.elements = deque()
+    def __init__(self, *elements):
+        self._elements = deque()
 
     def enqueue(self, element):
         self._elements.append(element)
 
     def dequeue(self):
         return self._elements.popleft()
-
-from queues import Queue
 
 fifo = Queue()
 fifo.enqueue("1st")
